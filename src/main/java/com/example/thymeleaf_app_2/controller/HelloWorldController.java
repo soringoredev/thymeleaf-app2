3b6750eeba90c231f9c99386e2fe87a1f3d55faa
Sorin Gore <soringoredev@gmail.com>
2025-06-14 14:59:15 +0300

    feat: Add HelloWorldController

---
+++ src/main/java/com/example/thymeleaf_app_2/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.thymeleaf_app_2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/showForm")
+    public String showForm() {
+        return "helloworld-form";
+    }
+
+}
